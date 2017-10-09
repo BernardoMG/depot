baa8173bf35ff2e37e6f2de5dbf313d5cbb909b9
Bernardo Marques Graça <bernardo.marquesg@gmail.com>
2017-10-09 16:46:36 +0100

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	#  Because the database stores just two digits after the decimal point, this would end up being zero in the database >= 0.01
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: { 
+		with: %r{\.(gif|jpg|png)\Z}i, 
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
 end
