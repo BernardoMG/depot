2d9a6d037e3943b238b020b9a3eec9e83bb9e4b4
Bernardo Marques Graça <bernardo.marquesg@gmail.com>
2017-10-12 11:39:59 +0100

    Cart added

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,17 @@ class Product < ApplicationRecord
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
 	validates :title, length: {minimum: 10, too_short: 'Title must be at least 10 characters.'}
+
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+	private
+
+	# ensure that there are no line items referencing this product
+	def ensure_not_referenced_by_any_line_item
+		unless line_items.empty?
+			errors.add(:base, 'Line Items Present')
+			throw :abort
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   root 'store#index', as: 'store_index' # define the store/index as root view for our app
 
   resources :products
