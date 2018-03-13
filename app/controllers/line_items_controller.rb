1623c67c915384682413c64146cfe44674e8792b
BernardoMG <bernardo.marquesg@gmail.com>
2018-03-13 18:08:28 +0000

    Added decrement button and logic

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
+  before_action :set_cart, only: [:create, :decrement]
   before_action :set_line_item, only: %i[show edit update destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_line_item
 
@@ -75,6 +75,24 @@ class LineItemsController < ApplicationController
           format.html { redirect_to @line_item.cart, notice: 'Item Removed' }
         end
         format.xml { head :ok }
+      end
+    end
+  end
+
+  # DECREMENT /line_items/1
+  # DECREMENT /line_items/1.json
+  def decrement
+    @line_item = @cart.decrement_line_item_quantity(params[:id])
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to store_index_url, notice: 'Line item was successfully updated.' }
+        format.js   { @current_item = @line_item }
+        format.json { render :show, status: :ok, location: @line_item }
+      else
+       format.html { render action: 'edit' }
+       format.js   { @current_item = @line_item }
+       format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
   end
